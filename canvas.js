07d3dc1f3868852eb7d3b9457e6ac5a7aebc7af1
Danny <Dtrumble12@gmail.com>
2021-02-09 16:19:54 -0600

    Copied to CDN code for pts.js, setup pts.js into the project, removed webpack config and npm stuff

--- canvas.js
+++ canvas.js
@@ -1,8 +1,11 @@
-import {CanvasSpace, Pt, Group} from "pts"
+Pts.namespace( window );
 
-const space = new CanvasSpace("#canvas");
-space.setup({ bgcolor: "#2a2c2b" });
 
-const form = space.getForm();
+var space = new CanvasSpace("#canvas");
+space.setup({ bgcolor: "#fff" });
 
-space.add(() => form.point( space.pointer, 10 ));
+var form = space.getForm();
+
+space.add( () => form.point( space.pointer, 10 ) );
+
+space.bindMouse().bindTouch().play();
