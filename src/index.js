19da8fdd075e1b9fc6a6eac31869c794ba52e2d7
Danny <Dtrumble12@gmail.com>
2021-03-22 21:11:31 -0500

    Cleaned up the mobile nav dropdown styling

--- src/index.js
+++ src/index.js
@@ -81,7 +81,7 @@ const onScrollInit = (elements) => {
 };
 
 const toggleMobileNav = () => {
-    const mobileNavElement = document.querySelector(".mobile-link-wrap");
+    const mobileNavElement = document.querySelector(".link-wrap");
     if (mobileNavElement.classList.contains("visible")) {
         mobileNavElement.classList.remove("visible");
     } else {
