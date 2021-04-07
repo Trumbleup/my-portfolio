fa7c7bd369e087959ac5b351adba4755be16c920
Danny <Dtrumble12@gmail.com>
2021-04-06 22:01:42 -0500

    Created the transformation effect on the slider when selecting the next or prev button

--- src/modules/setProjectButtons.js
+++ src/modules/setProjectButtons.js
@@ -23,15 +23,19 @@ const setProjectInfo = (id) => {
 const shiftSlide = (direction) => {
     const slider = document.querySelector(".slider");
     slider.classList.add("transition");
-    slider.style.transform = `translateX(${direction} * 700px)`;
-    if (direction == 1) {
-        document.querySelector(".slide:first-child").before(document.querySelector(".slide:last-child"));
-        console.log("hi");
-    } else if (direction == -1) {
-        document.querySelector(".slide:last-child").after(document.querySelector(".slide:first-child"));
-        console.log("-hi");
-    }
-    slider.classList.remove("transition");
+    slider.style.transform = `translateX(${direction * 700}px)`;
+    setTimeout(() => {
+        if (direction == 1) {
+            document.querySelector(".slide:first-child").before(document.querySelector(".slide:last-child"));
+            console.log("hi");
+        } else if (direction == -1) {
+            document.querySelector(".slide:last-child").after(document.querySelector(".slide:first-child"));
+            console.log("-hi");
+        }
+        slider.classList.remove("transition");
+        slider.style.transform = `translateX(0px)`;
+    }, 700);
+    
 }
 
 const setModal = () => {
