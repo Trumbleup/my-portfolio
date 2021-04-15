c5bf686ef45851456c56d14904688266d4509c9b
Danny <Dtrumble12@gmail.com>
2021-04-14 21:28:22 -0500

    Added an element that signals a success or error on form submission

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -1,11 +1,22 @@
 import axios from "axios";
 
+const formSuccess = (element) => {
+    element.textContent = "Thank you!";
+    element.classList = "confirm success";
+}
+
+const formError = (element) => {
+    element.textContent = "There was an error, please try again!";
+    element.classList = "confirm error";
+}
+
 const contact = async () => {
     const myForm = document.getElementById("contact-form");
     myForm.addEventListener("submit", (e) => {
         const name = document.querySelector("#name").value;
         const email = document.querySelector("#email").value;
         const message = document.querySelector("#message").value;
+        const confirm = document.querySelector(".confirm");
         e.preventDefault();
         axios({
             url: "https://formspree.io/f/xzbyzplk",
@@ -18,7 +29,8 @@ const contact = async () => {
                 email: email,
                 message: message
             }
-        }).then((response) => { console.log(response); })
+        }).then((response) => { formSuccess(confirm); })
+        .catch(error => { formError(confirm) });
     })
 }
 
