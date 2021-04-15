6755fd7aadb0c0f846aab82d5140bddcfde46948
Danny <Dtrumble12@gmail.com>
2021-04-15 18:13:37 -0500

    Created a pending icon that appears when handling the form functionality and waiting for a response

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -10,6 +10,24 @@ const formError = (element) => {
     element.classList = "confirm error";
 }
 
+const formPending = (element) => {
+    element.classList.add("pending");
+};
+
+const removeFormPending = (element) => {
+    element.classList.remove("pending");
+};
+
+const handlePending = () => {
+    const pendingIcon = document.querySelector("#contact i");
+    if (pendingIcon.classList.contains("pending")) {
+        removeFormPending(pendingIcon);
+    } else {
+        formPending(pendingIcon);
+    }
+};
+
+
 const contact = async () => {
     const myForm = document.getElementById("contact-form");
     myForm.addEventListener("submit", (e) => {
@@ -17,7 +35,9 @@ const contact = async () => {
         const email = document.querySelector("#email").value;
         const message = document.querySelector("#message").value;
         const confirm = document.querySelector(".confirm");
+        const pendingIcon = document.querySelector("#contact i");
         e.preventDefault();
+        handlePending();
         axios({
             url: "https://formspree.io/f/xzbyzplk",
             method: "post",
@@ -29,9 +49,16 @@ const contact = async () => {
                 email: email,
                 message: message
             }
-        }).then((response) => { formSuccess(confirm); })
-        .catch(error => { formError(confirm) });
-    })
-}
+        })
+        .then((response) => {
+                handlePending();
+                formSuccess(confirm); 
+        })
+        .catch(error => {
+                handlePending();
+                formError(confirm);
+        });
+    });
+};
 
 export default contact;
