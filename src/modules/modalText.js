b3568ee4d2f5816901fd12f4465b21da4cf18489
Danny <Dtrumble12@gmail.com>
2021-06-11 13:32:38 -0500

    Added my weather application to the portfolio

--- src/modules/modalText.js
+++ src/modules/modalText.js
@@ -4,6 +4,9 @@ import fruitionImg2 from "../assets/images/slides/fruition-slide-2.PNG";
 import jsleeveImg0 from "../assets/images/slides/jsleeve-slide-0.PNG";
 import jsleeveImg1 from "../assets/images/slides/jsleeve-slide-1.PNG";
 import jsleeveImg2 from "../assets/images/slides/jsleeve-slide-2.PNG";
+import weatherAppImg0 from "../assets/images/slides/weatherApp-slide-0.PNG";
+import weatherAppImg1 from "../assets/images/slides/weatherApp-slide-1.PNG";
+import weatherAppImg2 from "../assets/images/slides/weatherApp-slide-2.PNG";
 
 const modalText = {
     fruition: {
@@ -26,6 +29,17 @@ const modalText = {
             jsleeveImg0,
             jsleeveImg1,
             jsleeveImg2
+        ]
+    },
+    weatherApp: {
+        title: "Weather App",
+        info: "Personal Project for The Odin Project",
+        details: "Developed using ReactJS, Hooks, and Asynchronus Programming",
+        link: "https://trumbleup.github.io/weather-app/",
+        images: [
+            weatherAppImg0,
+            weatherAppImg1,
+            weatherAppImg2
         ]
     }
 };
