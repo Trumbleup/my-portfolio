aff7a7b2d1a19eb5b39ccda8d7d96aa4009f7afa
Danny <Dtrumble12@gmail.com>
2021-07-22 22:37:04 -0500

    Added images and data for battleship and crwn clothing projects

--- src/modules/modalText.js
+++ src/modules/modalText.js
@@ -10,12 +10,18 @@ import weatherAppImg2 from "../assets/images/slides/weatherApp-slide-2.PNG";
 import robofriendsImg0 from "../assets/images/slides/robofriends-slide-0.PNG";
 import robofriendsImg1 from "../assets/images/slides/robofriends-slide-1.PNG";
 import robofriendsImg2 from "../assets/images/slides/robofriends-slide-2.PNG";
+import crwnClothingImg0 from "../assets/images/slides/crwn-clothing-slide-0.PNG";
+import crwnClothingImg1 from "../assets/images/slides/crwn-clothing-slide-1.PNG";
+import crwnClothingImg2 from "../assets/images/slides/crwn-clothing-slide-2.PNG";
+import battleshipImg0 from "../assets/images/slides/battleship-slide-0.PNG";
+import battleshipImg1 from "../assets/images/slides/battleship-slide-1.PNG";
+import battleshipImg2 from "../assets/images/slides/battleship-slide-2.PNG";
 
 const modalText = {
     fruition: {
         title: "Fruition Tech Labs",
         info: "Business and Technology Commercialization Company",
-        details: "Developed using HTML5, CSS3, Javascript, React.js, Bootstrap",
+        details: "Developed using HTML5, CSS3, Javascript, React.js, Bootstrap.",
         link: "http://www.fruitiontechlabs.com/#/",
         images: [
             fruitionImg0,
@@ -26,7 +32,7 @@ const modalText = {
     jsleeve: {
         title: "Synergy Svn",
         info: "Athletic Tech Company",
-        details: "Developed using React.js, Next.js, GraphQL, Node.js, PostgreSQL. Design by Brooke Adams",
+        details: "Developed using React.js, Next.js, GraphQL, Node.js, PostgreSQL. Design by Brooke Adams.",
         link: "https://www.jsleeve.com/",
         images: [
             jsleeveImg0,
@@ -37,7 +43,7 @@ const modalText = {
     weatherApp: {
         title: "Weather App",
         info: "Personal Project for The Odin Project",
-        details: "Developed using ReactJS, Hooks, and Asynchronus Programming",
+        details: "Developed using ReactJS, Hooks, and Asynchronus Programming.",
         link: "https://trumbleup.github.io/weather-app/",
         images: [
             weatherAppImg0,
@@ -48,12 +54,34 @@ const modalText = {
     robofriends: {
         title: "Robofriends",
         info: "Project from the Zero to Mastery course by Andrei Neogie",
-        details: "Developed using React.js",
+        details: "Developed using React.js.",
         link: "https://trumbleup.github.io/robofriends/",
         images: [
             robofriendsImg0,
             robofriendsImg1,
             robofriendsImg2
+        ]
+    },
+    crwnClothing: {
+        title: "Crwn-Clothing",
+        info: "Project from the React 2021 course by Andrei Neogie",
+        details: "Developed using React.js, Redux, and Firebase. Please click the Crown Icon to render the main page!",
+        link: "https://trumbleup.github.io/crwn-clothing/",
+        images: [
+            crwnClothingImg0,
+            crwnClothingImg1,
+            crwnClothingImg2
+        ]
+    },
+    battleship: {
+        title: "Battleship",
+        info: "Project from The Odin Project curriculum to practice testing with Jest.",
+        details: "Developed using React.js and Jest.",
+        link: "https://trumbleup.github.io/battleship/",
+        images: [
+            battleshipImg0,
+            battleshipImg1,
+            battleshipImg2
         ]
     }
 };
